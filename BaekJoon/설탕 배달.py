5fab9e1fe73b469e357aa04c1578c16b50493a59
Hanna <58249219+hanna56@users.noreply.github.com>
2021-10-03 00:09:23 +0900

    [백준 2839] - 설탕 배달

---
+++ BaekJoon/설탕 배달.py
@@ -0,0 +1,18 @@
+N = int(input())
+answer = 0
+
+if N%5==0:
+  print(N//5)
+else:
+  answer = 0
+  while N>=5:
+    if (N-3) % 5 == 0:
+      answer+=N//5+1
+      N = (N-3) % 5
+    else:
+      N-=3
+      answer+=1
+  print(answer+N//3) if N%3==0 else print(-1)
+
+  
+  
