90327a57a973fbc86cd8c4dc86872f98e898d908
Hanna <58249219+hanna56@users.noreply.github.com>
2021-10-14 21:03:08 +0900

    [백준-11053] 가장 긴 증가하는 부분 수열

---
+++ BaekJoon/가장 긴 증가하는 부분 수열.py
@@ -0,0 +1,10 @@
+x = int(input())
+arr = list(map(int, input().split()))
+
+dp = [1] * x
+
+for i in range(1, x):
+  for j in range(i):
+    if arr[i] > arr[j]:
+      dp[i] = max(dp[i], dp[j]+1)
+print(max(dp))
